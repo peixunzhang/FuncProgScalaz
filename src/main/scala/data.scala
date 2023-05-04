3cc0477e24fc47146c4e5ff5368380f973ad40ca
Peixun Zhang <max317backup@gmail.com>
2023-05-05 00:27:51 +0800

    instances

--- src/main/scala/data.scala
+++ src/main/scala/data.scala
@@ -91,7 +91,7 @@ object RefreshResponse {
 }
 
 object UrlEncodedWriter {
-  implicit val encoder: UrlEncodedWriter[String Refined Url] = identity
+  // implicit val encoder: UrlEncodedWriter[String Refined UrlEncoded] = identity
   implicit val string: UrlEncodedWriter[String] =
     (s => Refined.unsafeApply(URLEncoder.encode(s, "UTF-8")))
   implicit val url: UrlEncodedWriter[String Refined Url] = 
@@ -103,5 +103,42 @@ object UrlEncodedWriter {
       case (k, v) => k.toUrlEncoded.value + "=" + v.toUrlEncoded.value
     }.intercalate("&")
     Refined.unsafeApply(raw)
+  }
+}
+
+object AuthRequest {
+  implicit val query: UrlQueryWriter[AuthRequest] = { a => 
+    UrlQuery(List(
+      ("redirect_uri" -> a.redirect_uri.value),
+      ("scope" -> a.scope),
+      ("client_id" -> a.client_id),
+      ("prompt" -> a.prompt),
+      ("response_type" -> a.response_type),
+      ("access_type" -> a.access_type)
+    ))
+  }
+}
+
+object AccessRequest {
+  implicit val encoded: UrlEncodedWriter[AccessRequest] = { a =>
+    IList(
+      "code" -> a.code.toUrlEncoded,
+      "redirect_uri" -> a.redirect_uri.toUrlEncoded,
+      "client_id" -> a.client_id.toUrlEncoded,
+      "client_secret" -> a.client_secret.toUrlEncoded,
+      "scope" -> a.scope.toUrlEncoded,
+      "grant_type" -> a.grant_type.toUrlEncoded
+    ).toUrlEncoded
+  }
+}
+
+object RefreshRequest {
+  implicit val encoded: UrlEncodedWriter[RefreshRequest] = { r =>
+    IList(
+      "client_secret" -> r.client_secret.toUrlEncoded,
+      "refresh_token" -> r.refresh_token.toUrlEncoded,
+      "client_id" -> r.client_id.toUrlEncoded,
+      "grant_type" -> r.grant_type.toUrlEncoded
+    ).toUrlEncoded
   }
 }
